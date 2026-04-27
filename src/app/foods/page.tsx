907d50270ac56ca6498db6ef4b3344a1771dcec2
Xian Saiful <xiansaiful@gmail.com>
2026-04-27 23:52:59 +0600

    feat: implement getFoods function to fetch random food data

--- src/app/foods/page.tsx
+++ src/app/foods/page.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+const getFoods = () => {
+    const res = await fetch("https://taxi-kitchen-api.vercel.app/api/v1/foods/random")
+}
+
 const page = () => {
     return (
         <div>page</div>
