9579222518fbdb6d03879fe581b4ebb644c54a46
Xian Saiful <xiansaiful@gmail.com>
2026-04-28 00:39:04 +0600

    feat: center loading spinner within the container for improved UI alignment

--- src/app/foods/loading.tsx
+++ src/app/foods/loading.tsx
@@ -1,11 +1,9 @@
-import { Field, FieldLabel } from '@/components/ui/field'
-import { Progress } from '@/components/ui/progress'
 import { Spinner } from '@/components/ui/spinner'
 import React from 'react'
 
 export default function loading() {
     return (
-        <div className='p-10'>
+        <div className='p-10 mx-auto'>
             <Spinner />
         </div>
     )
