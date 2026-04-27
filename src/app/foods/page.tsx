090c2535582dbde350c9bb1361ff3df14c775bb0
Xian Saiful <xiansaiful@gmail.com>
2026-04-27 23:54:38 +0600

    feat: enhance getFoods function to return food data from API

--- src/app/foods/page.tsx
+++ src/app/foods/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const getFoods = async () => {
-    const res = await fetch("https://taxi-kitchen-api.vercel.app/api/v1/foods/random")
-}
+    const res = await fetch("https://taxi-kitchen-api.vercel.app/api/v1/foods/random");
+    const data = await res.json();
+    return data.foods || [];
+};
 
 const page = () => {
     return (
